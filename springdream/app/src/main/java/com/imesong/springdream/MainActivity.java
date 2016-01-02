c58a527d0b4e2c8bcf78c4869a459572a432ab2c
imesong <imesong@126.com>
2016-01-02 14:04:40 +0800

    add some libs

--- springdream/app/src/main/java/com/imesong/springdream/MainActivity.java
+++ springdream/app/src/main/java/com/imesong/springdream/MainActivity.java
@@ -1,15 +1,28 @@
 package com.imesong.springdream;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PrimaryDrawerItem item1 ;
+    private SecondaryDrawerItem item2;
+    private Drawer mLeftDrawer ;
+    private Drawer mRightDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +31,74 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
+
+        initDrawer(toolbar);
+
+
     }
+
+
+    private void initDrawer(Toolbar toolbar){
+
+        item1 = new PrimaryDrawerItem().withName(R.string.item1);
+        item2 = new SecondaryDrawerItem().withName(R.string.item2);
+        mLeftDrawer = new DrawerBuilder().withActivity(this).withToolbar(toolbar).
+            addDrawerItems(item1,new DividerDrawerItem(),item2,new SecondaryDrawerItem().withName(R.string.item3)).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                return false;
+            }
+        }).build();
+
+
+
+        //now we add the second drawer on the other site.
+        //use the .append method to add this drawer to the first one
+        mRightDrawer = new DrawerBuilder()
+            .withActivity(this)
+            .withDisplayBelowStatusBar(true)
+            .addDrawerItems(
+                new PrimaryDrawerItem().withName(R.string.right_item1).withIcon(FontAwesome.Icon.faw_eye),
+                new PrimaryDrawerItem().withName(R.string.right_item2).withIcon(FontAwesome.Icon.faw_home),
+                new PrimaryDrawerItem().withName(R.string.right_item3).withIcon(FontAwesome.Icon.faw_gamepad),
+                new SectionDrawerItem().withName(R.string.right_item4),
+                new SecondaryDrawerItem().withName(R.string.right_item5).withIcon(FontAwesome.Icon.faw_cog),
+                new DividerDrawerItem(),
+                new SecondaryDrawerItem().withName(R.string.right_item6).withIcon(FontAwesome.Icon.faw_github),
+                new SecondaryDrawerItem().withName(R.string.right_item7).withIcon(FontAwesome.Icon.faw_question).withEnabled(false),
+                new SectionDrawerItem().withName(R.string.right_item8)
+            )
+            .withDrawerGravity(Gravity.END)
+            .append(mLeftDrawer);
+
+
+        //set the back arrow in the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
