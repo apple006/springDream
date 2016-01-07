09a7c8e95f3d9fc9543ddf80c6729225ac0cd91a
imesong <imesong@126.com>
2016-01-07 21:21:25 +0800

    add greenDaoGenerate libaray  and database entity

--- springdream/greendaogenerate/src/main/java/com/imesong/tools/greendao/GreenDaoGenerate.java
+++ springdream/greendaogenerate/src/main/java/com/imesong/tools/greendao/GreenDaoGenerate.java
@@ -1,26 +1,53 @@
 package com.imesong.tools.greendao;
 
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
 public class GreenDaoGenerate {
 
     private static final int DB_VERSION = 20160101;
-    private static final String PACKAGE_NAME = "com.imesong.springdream";
+    private static final String PACKAGE_NAME = "com.imesong.springdream.database";
+    private static final String SOURCE_PATH = "../springdream/app/src/main/java";
 
     public static void main(String[] args){
+        Schema schema = new Schema(DB_VERSION,PACKAGE_NAME);
+        createCategoryTable(schema);
+        createDreamTable(schema);
+        generateDaoFile(schema);
+    }
+    private static void createCategoryTable(Schema schema){
+        Entity category = schema.addEntity("Category");
+        category.addIdProperty();
+        category.addStringProperty("name").notNull();
+        category.addStringProperty("parent");
+        category.addStringProperty("pinxin");
+        category.addShortProperty("sequence");
+        category.addStringProperty("del");
+    }
 
+    private static void createDreamTable(Schema schema){
+        Entity dream = schema.addEntity("Dream");
+        dream.addIdProperty();
+        dream.addStringProperty("category1");
+        dream.addStringProperty("category2");
+
+        dream.addStringProperty("name").notNull();
+        dream.addStringProperty("content").notNull();
+        dream.addStringProperty("del");
+        dream.addStringProperty("recommend");
+        dream.addStringProperty("sequence");
+        dream.addStringProperty("hit");
+        dream.addIntProperty("is_long");
 
     }
 
-
-
-    private static void createCategoryTable(){
-
-
+    private static void generateDaoFile(Schema schema){
+        try {
+            new DaoGenerator().generateAll(schema, SOURCE_PATH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    private static void createDreamTable(){
-
-    }
-
-    private static void generateDaoFile(){}
 
 }
