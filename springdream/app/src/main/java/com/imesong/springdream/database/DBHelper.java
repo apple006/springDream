766ac39c975ff815c0f96a2cbf80eb894215454f
imesong <imesong@126.com>
2016-01-07 09:11:20 +0800

    add greenDAO generate library and assets database

---
+++ springdream/app/src/main/java/com/imesong/springdream/database/DBHelper.java
@@ -0,0 +1,31 @@
+package com.imesong.springdream.database;
+
+import android.content.Context;
+
+/**
+ * 数据库操作工具类
+ */
+public class DBHelper {
+    public static DBHelper instance = null;
+
+    public static DBHelper getInstance(Context context){
+        if (instance == null){
+            instance = new DBHelper(context);
+        }
+
+        return  instance;
+    }
+
+
+
+    private DBHelper(Context context){
+//        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, DB_NAME, null);
+//
+//        DaoMaster daoMaster = new DaoMaster(helper.getWritableDatabase());
+//
+//        daoSession = daoMaster.newSession();
+//        asyncSession = daoSession.startAsyncSession();
+    }
+
+
+}
---
+++ springdream/greendaogenerate/src/main/java/com/imesong/tools/greendao/GreenDaoGenerate.java
@@ -0,0 +1,26 @@
+package com.imesong.tools.greendao;
+
+public class GreenDaoGenerate {
+
+    private static final int DB_VERSION = 20160101;
+    private static final String PACKAGE_NAME = "com.imesong.springdream";
+
+    public static void main(String[] args){
+
+
+    }
+
+
+
+    private static void createCategoryTable(){
+
+
+    }
+
+    private static void createDreamTable(){
+
+    }
+
+    private static void generateDaoFile(){}
+
+}
